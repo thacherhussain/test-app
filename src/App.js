3a42882b70b607d7be8617b061432f8f39277a74
Thacher Hussain <thacher.hussain@gmail.com>
2021-01-10 17:31:57 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const totalCards = 50;
+
+function Greeting(totalCards) {
+  if (totalCards < 10) {
+    return <span>you should open more packs</span>;
+  } else {
+    return <span>you have a lot of cards! trade them for more!</span>;
+  }
+}
+
+const input = "Crawford";
+
+function UserName(input) {
+  return <span>`My Name is {input}`</span>;
+}
+
+const CardNumber = () => {
+  return (
+    <>
+      <div>I have {totalCards} cards</div>
+      <div>
+        I have {totalCards} cards {Greeting()}
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <UserName /> */}
+      <CardNumber />
     </div>
   );
 }
